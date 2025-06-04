62df38a23d8e10112c5196f2fcce9a2269fb1fdd
quockhanhs <chuquoc591@gmail.com>
2025-06-04 23:43:01 +0700

    dasdsadsads

--- src/app.js
+++ src/app.js
@@ -43,13 +43,16 @@ app.use(express.json({ limit: "5mb" }));
 app.use(express.urlencoded({ extended: true, limit: "5mb" }));
 app.use(
   cors({
-    credentials: true,
     origin: [
       "http://localhost:5173", // Địa chỉ FE local
       "https://datn-server-c9cu.onrender.com", // Địa chỉ BE trên Render
     ],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
   })
 );
+
 app.use(helmet());
 app.use(compression());
 app.use(morgan("dev"));
