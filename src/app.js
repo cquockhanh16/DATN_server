45ea6e9e7b9380fee0c053645eb81f72834f866e
quockhanhs <chuquoc591@gmail.com>
2025-06-04 23:36:18 +0700

    dasdsadsad

--- src/app.js
+++ src/app.js
@@ -41,7 +41,15 @@ app.set("trust proxy", true);
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json({ limit: "5mb" }));
 app.use(express.urlencoded({ extended: true, limit: "5mb" }));
-app.use(cors({ credentials: true }));
+app.use(
+  cors({
+    credentials: true,
+    origin: [
+      "http://localhost:5173", // Địa chỉ FE local
+      "https://datn-server-c9cu.onrender.com", // Địa chỉ BE trên Render
+    ],
+  })
+);
 app.use(helmet());
 app.use(compression());
 app.use(morgan("dev"));
