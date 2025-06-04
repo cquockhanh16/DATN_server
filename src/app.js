f4cee3502a08dc6ac570b7ee64378b452ac372dd
quockhanhs <chuquoc591@gmail.com>
2025-06-04 23:55:22 +0700

    dasdasdsa

--- src/app.js
+++ src/app.js
@@ -47,7 +47,7 @@ app.use(
       "http://localhost:5173", // Địa chỉ FE local
       "https://datn-server-c9cu.onrender.com", // Địa chỉ BE trên Render
     ],
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
   })
